63084b38ca0df7efb4a7a5afa9d65b66c3d966d4
doni-zete <donizete112@gmail.com>
2023-07-02 20:48:56 -0300

    conexao com db

---
+++ src/database/database.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose")
+
+function connectToDatabase(){
+    mongoose.connect("mongodb://localhost:27017", {
+        useNewUrlParser: true,
+        useUnifiedTopology:true,
+    }).then(()=>{
+        console.log("Mongo Db conectado")
+    }).catch((err)=>{
+        return console.log(`Erro na conexao com o banco: ${err}`)
+    })
+
+}
+
+module.exports = connectToDatabase
---
+++ src/model/Usuario.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+
+const UsuarioSchema = new mongoose.Schema({
+  nome: { type: String, required: true },
+  email: { type: String, unique: true, require: true },
+  senha: { type: String, required: true },
+  imagem: { type: String, required: true },
+  endereco: [
+    {
+      rua: { type: String, required: true },
+      numero: { type: String, required: true },
+      complemento: { type: String, required: true },
+      CEP: { type: String, required: true },
+      creadAt: { type: Date, required: true },
+    },
+  ],
+  creadAt: { type: Date, required: true },
+  produtos_fav: [
+    {
+      _id: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        unique: true,
+        ref: "produtos",
+        creadAt: { type: Date, required: true },
+      },
+    },
+  ],
+  admin: { type: Boolean, required: true, default: false },
+});
+
+
+const Usuario = mongoose.model("usuarios",UsuarioSchema)
+module.exports = Usuario;

052886b224d925b1555726cd593975233db3265e
doni-zete <donizete112@gmail.com>
2023-07-04 20:31:59 -0300

    feat: scripts, rotas, usuarios e controller

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const connectToDatabase = require("./src/database/database");
-
+// importando a rotas
+const usuario = require("./src/router/usuario.router")
 
 const app = express();
-
 const port = 3000;
 
 app.use(express.json());
+
+app.use("/usuario", usuario)
 
 connectToDatabase()
 

d5926b3a1a3351fd2ec706ef958b0fcbaf1ed473
doni-zete <donizete112@gmail.com>
2023-07-09 16:19:41 -0300

    autorização middleware

--- src/router/usuario.router.js
+++ src/router/usuario.router.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const usuarioController = require("../controller/usuario.controller");
-
+const authMiddleware = require("../middleware/auth.middleware")
 
 // rotas get
-router.get("/findById/:id", usuarioController.findUserByIdController);
-router.get("/findAll", usuarioController.findAllUsersController);
+router.get("/findById/:id", authMiddleware, usuarioController.findUserByIdController);
+router.get("/findAll", authMiddleware, usuarioController.findAllUsersController);
 
 // rotas POST
 router.post("/create", usuarioController.createUserController);

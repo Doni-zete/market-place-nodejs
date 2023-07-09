f68a7ced6ffa5d477315025454e4c285185d5d5e
doni-zete <donizete112@gmail.com>
2023-07-09 14:39:25 -0300

    Feat: Autenticação funcionando

--- index.js
+++ index.js
@@ -2,13 +2,17 @@ const express = require("express");
 const connectToDatabase = require("./src/database/database");//arquivo de conexao com o banco
 // importando a rotas
 const usuario = require("./src/router/usuario.router")//arquivo de rota do usuario
+const auth = require("./src/router/auth.router")//arquivo de rota do usuario
+
 
 const app = express();
 const port = 3000;
 
 app.use(express.json());
 
-app.use("/usuario", usuario)
+app.use("/usuario", usuario)//chamando as rotas usuario
+app.use("/auth", auth) //chamando as rotas auth
+
 
 connectToDatabase() //conectadno com o banco
 
---
+++ src/service/auth.service.js
@@ -0,0 +1,17 @@
+const Usuario = require("../model/Usuario");
+const jwt = require("jsonwebtoken");
+
+const loginService = (email) =>
+  Usuario.findOne({ email: email }).select("senha");
+
+const generateToken = (userId) =>
+  jwt.sign(
+    { id: userId },
+    "$2b$10$qOvn14sswFAmiDI/5FDewRBAOV.vNMAJKRCE10Qu2lPVyMaNF2KtJZB6",
+    { expiresIn: 86400 }
+  );
+
+module.exports = {
+  loginService,
+  generateToken,
+};

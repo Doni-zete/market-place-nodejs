294786ea367e1a0e6dd1a2bf60e93a9047f02747
doni-zete <donizete112@gmail.com>
2023-07-10 23:38:30 -0300

    feat crud categoria

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const connectToDatabase = require("./src/database/database"); //arquivo de conex
 // importando a rotas
 const usuario = require("./src/router/usuario.router"); //arquivo de rota do usuario
 const produto = require("./src/router/produto.router"); //arquivo de rota do produto
+const categoria = require("./src/router/categoria.router"); //arquivo de rota do categoria
 const auth = require("./src/router/auth.router"); //arquivo de rota do usuario
+
 
 const app = express();
 const port = 3000;
@@ -13,6 +15,8 @@ app.use(express.json());
 
 app.use("/usuario", usuario); //chamando as rotas usuario
 app.use("/produto", produto); //chamando as rotas usuario
+app.use("/categoria", categoria); //chamando as rotas categoria
+
 app.use("/auth", auth); //chamando as rotas auth
 
 connectToDatabase(); //conectadno com o banco
--- src/router/produto.router.js
+++ src/router/produto.router.js
@@ -3,10 +3,10 @@ const router = require("express").Router();
 const produtoController = require("../controller/produto.controller");
 const authMiddleware = require("../middleware/auth.middleware")
 
-router.all("/find/:id", authMiddleware, produtoController.findProductByIdController)
-router.all("/findAll", authMiddleware, produtoController.findAllProductController)
-router.all("/create", authMiddleware, produtoController.createProductController)
-router.all("/update/:id", authMiddleware, produtoController.updateProductController)
-router.all("/delete/:id", authMiddleware, produtoController.deleteProductController)
+router.get("/find/:id", authMiddleware, produtoController.findProductByIdController)
+router.get("/findAll", authMiddleware, produtoController.findAllProductController)
+router.post("/create", authMiddleware, produtoController.createProductController)
+router.put("/update/:id", authMiddleware, produtoController.updateProductController)
+router.delete("/delete/:id", authMiddleware, produtoController.deleteProductController)
 
 module.exports = router;
